174da3dec0ca95e5cf61a7fb3eea7c92e4d37495
Earl Cameron <mr.e.cameron@gmail.com>
2021-11-02 00:57:52 -0400

    introduction component start

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import React from "react";
 
 // import components
 import Nav from "../components/navbar/index";
+import Introduction from "../components/homeIntroduction/index";
 import Feature from "../components/landingFeature/index";
 
 const Home = () => {
   return (
     <>
       <Nav />
+      <Introduction />
       <Feature />
     </>
   );
