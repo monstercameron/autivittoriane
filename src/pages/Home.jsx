5e0722c4dbdb4e32908d9f1d9cc5ea2b88dd6d04
Earl Cameron <mr.e.cameron@gmail.com>
2021-11-01 22:35:00 -0400

    init commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import {Database} from "../database/database.jsx";
+
+// import components
+import Nav from "../components/navbar/index";
+
+const Home = () => {
+  return (
+    <>
+      <Nav />
+      <div className="container">home</div>
+    </>
+  );
+};
+
+export default Home;
